e20ba0ef4ac1d8b7199bf786ed400d6aa9e8d513
miumiu <pavlovNP1992@yandex.ru>
2022-07-14 18:18:34 +0300

    ПОбеда

---
+++ Кресты.py
@@ -0,0 +1,62 @@
+#игравое поле
+field = [['-']*3 for _ in range(3)]
+#функция вывода поля
+def show_field(field):
+    print('  0 1 2')
+    for i in range(len(field)):
+        print(str(i) + ' ' + ' '.join(field[i]))
+#функция ввода координат
+def user_input(field):
+    while True:
+        place= input('введите координаты :').split()
+        if len(place)!=2:
+            print("введите две координаты")
+            continue
+        if  not(place[0].isdigit() and place[1].isdigit()):
+            print('введите числа')
+            continue
+        x,y=map(int,place)
+        if not (3>x>=0 and 3>y>=0):
+            print('Вышли из диапозона')
+            continue
+        if field[x][y] != '-':
+            print('клетка занята')
+            continue
+        break
+    return x,y
+
+count = 0
+#условия победы
+def win(f,user):
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+#основной цикл игры
+while True:
+    if count == 9:
+        print('Ничья')
+        break
+    if count%2 == 0:
+        user = 'X'
+    else:
+        user = '0'
+    show_field(field)
+    x,y=user_input(field)
+    field[x][y]=user
+    count+=1
+    if win(field,user):
+        print(f'viktory {user}')
+        break
+    print('количество ходов',count)
+
